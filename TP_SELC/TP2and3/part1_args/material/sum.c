f9183d02506be48c32f4bee67aa1fb83fb5cbb12
Tristan Perrot <tristan.perrot@telecom-paris.fr>
2022-01-24 16:15:03 +0100

    part1 tp2

--- TP_SELC/TP2and3/part1_args/material/sum.c
+++ TP_SELC/TP2and3/part1_args/material/sum.c
@@ -6,12 +6,27 @@ void display_message(char * message)
   printf(message);
 }
 
-int main(int argc, char *argv[]) {
-  /* to be completed */
-  // next line just shows how to use display_message; could be removed
-  display_message("a basic message\n");
+// declare function before using it
+char isCharInteger(char c);
 
+int main(int argc, char * argv[]) {
   // first, check that the number of parameters is correct
+  if(argc != 3) {
+    display_message("Wrong usage, 2 parameters expected: ./sum param1 param2\n");
+  } else {
+    int i;
+    for(i = 1; i < argc; i++) {
+      char * chaine = argv[i];
+      while(*chaine != '\0') {
+        if(!isCharInteger(*chaine)) {
+          display_message("Wrong usage, parameters param1 and param2 should be integers when executing ./sum param1 param2\n");
+        } else {
+          display_message("a basic message\n");
+        }
+      chaine++;
+      }
+    }
+  }
 
   // now, check that parameters represent integer values
   // declare, implement, and use function isCharInteger
@@ -21,6 +36,18 @@ int main(int argc, char *argv[]) {
 
 char isCharInteger(char c)
 {
-  /* to be completed */
+  switch (c) {
+    case '0' :
+    case '1' :
+    case '2' :
+    case '3' :
+    case '4' :
+    case '5' :
+    case '6' :
+    case '7' :
+    case '8' :
+    case '9' :
+      return 1;
+  }
   return 0;
 }

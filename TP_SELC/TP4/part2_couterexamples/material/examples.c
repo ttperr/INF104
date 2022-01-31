94c4b9bac1d46da3e9d9a5949e203c56744ea6fa
Tristan Perrot <tristan.perrot@telecom-paris.fr>
2022-01-31 16:35:24 +0100

    fin tp4

--- TP_SELC/TP4/part2_couterexamples/material/examples.c
+++ TP_SELC/TP4/part2_couterexamples/material/examples.c
@@ -1,28 +1,31 @@
 #include <stdio.h>
 
-void init(int * tab);
-void swap(float f1, float f2);
+int * alloc(int val);
+void swap(float * f1, float * f2);
 
 int main(int argc, char *argv[]) {
   int * ptr = alloc(7);
   // use printf to look at the content accessible from ptr
+  printf(ptr);
 
   float f1=0.7;
   float f2=1.2;
   // use swap to put value of f1 in f2 and vice-versa
   // use printf to display values of f1 and f2
-  
+  swap(&f1, &f2);
   return 0;
 }
 
-int * alloc (int val){
-   int tab[20];
-   // initialize elements of tab with value val
-
-   return tab;
+int * alloc (int val) {
+  int * tab;
+  // initialize elements of tab with value val
+  tab = (int *) malloc(20*sizeof(int)); 
+  }
+  return tab;
 }
 
-void swap(float f1, float f2)
-{
-
+void swap(float * f1, float * f2) {
+  float t = *f1;
+  *f1 = *f2;
+  *f2 = t;
 }

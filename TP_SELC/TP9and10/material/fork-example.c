e83ac8288a49d7e949ad8e2ea54dc0c4fd1c5df3
Tristan Perrot <tristan.perrot@telecom-paris.fr>
2022-02-22 09:57:22 +0100

    format

--- TP_SELC/TP9and10/material/fork-example.c
+++ TP_SELC/TP9and10/material/fork-example.c
@@ -6,7 +6,9 @@ int main (){
   pid_t ret;
   printf("printf-0 Before fork : executing process number: %d\n", (int)getpid());
   ret = fork();
+  if (ret == 0) sleep (4);
   printf("printf-1 Forked, returned value is: %d\n", (int)ret);
+  sleep(0);
   printf("printf-2 Executing process number: %d\n", (int)getpid());
   return 0;
 }

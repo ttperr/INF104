001d5e33e1e86bd19a6fabb9fe6d61cda989eceb
Tristan <tristan.perrot@telecom-paris.fr>
2022-04-08 09:21:21 +0200

    error_recovery encoded

--- TP_SELC/TP13/material/TP13_ign_all.c
+++ TP_SELC/TP13/material/TP13_ign_all.c
@@ -1,17 +1,27 @@
 #include <stdio.h>
 #include <signal.h>
 #include <unistd.h>
-int main(int argc, char *argv[]){
+
+void on_signal_reception(int);
+
+int main(int argc, char *argv[])
+{
   int Nb_Sig;
-  printf("NSIG = %d", NSIG);
+  printf("NSIG = %d\n", NSIG);
   sleep(3);
-  for(Nb_Sig = 1; Nb_Sig < NSIG ; Nb_Sig ++)
+  for (Nb_Sig = 1; Nb_Sig < NSIG; Nb_Sig++)
   {
-   
-  } 
-  while(1)
+    if (signal(Nb_Sig, SIG_IGN) == SIG_ERR)
+      printf("signal(%d, SIG_IGN) failed\n", Nb_Sig);
+  }
+  while (1)
   {
     sleep(5);
   } /* Wait for receiving signals */
   return 0;
+}
+
+void on_signal_reception(int sig_number)
+{
+  printf("Here we go, signal %d received!\n", sig_number);
 }
